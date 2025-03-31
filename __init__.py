4538f55b1857384200fb1663ccb019f32cc79af9
Molka-Belakhdar <molkabellakhdher@gmail.com>
2025-03-31 11:51:39 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -22,14 +22,6 @@ from flask import json
 app = Flask(__name__)                                                                                                                  
 
 
-@app.route('/<int:valeur>')
-def exercice(valeur):
-    etoiles = ''
-    for i in range(valeur):
-        etoiles += '*'
-        for i in 
-    return etoiles #comm
-
 
 if __name__ == "__main__":
   app.run(debug=True)
