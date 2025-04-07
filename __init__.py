862366866580ce046fdbfafda23db72a3174508a
Molka-Belakhdar <molkabellakhdher@gmail.com>
2025-04-07 11:25:29 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -2,19 +2,21 @@ from flask import Flask
 from flask import render_template
 from flask import json
 
+
 app = Flask(__name__)
 
 @app.route('/<int:valeur>')
 def exercice(valeur):
-    if valeur < 2:
-        return '<pre>La valeur doit être supérieure ou égale à 2.</pre>'
+    somme = 0
+    for i in range(1, valeur + 1):
+        if i % 11 == 0:
+            continue
+        if i % 5 == 0 or i % 7 == 0:
+            if somme + i > 5000:
+                break
+            somme += i
     
-    sequence = [0, 1]
-    for _ in range(2, valeur):
-        sequence.append(sequence[-1] + sequence[-2])
-    
-    resultat = '<pre>' + ', '.join(map(str, sequence)) + '</pre>'
-    return resultat
+    return f'<pre>Somme finale: {somme}</pre>'
 
 if __name__ == "__main__":
     app.run(debug=True)
