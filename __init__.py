ee853cb7c6ce5c3fa17158c48ebae9cb70270251
Molka-Belakhdar <molkabellakhdher@gmail.com>
2025-04-07 09:58:45 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -8,10 +8,10 @@ app = Flask(__name__)
 def exercice(valeur):
     a, b = 0, 1
     sequence = [a, b]
-    for  in range(2, valeur):
+    for _ in range(2, valeur):
         a, b = b, a + b
         sequence.append(b)
-
+    
     fibonacci = '<pre>' + ', '.join(map(str, sequence)) + '</pre>'
     return fibonacci
 
